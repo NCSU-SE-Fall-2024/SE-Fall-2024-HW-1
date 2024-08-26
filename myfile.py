90e90fab7c932e650d70c5918664f576376e01d2
ShanmukhaSreenivas <97184144+ShanmukhaSreenivas@users.noreply.github.com>
2024-08-26 23:52:50 +0000

    Ran the python code with error

--- myfile.py
+++ myfile.py
@@ -1,10 +1,7 @@
-def maximum(a, b):    
-    if a >= b:
-        return a
-    else:
-        return b
-    
 a = 2
 b = 4
-c = 6
-print(maximum(a, b, c))
+
+if a >= b:
+    print(a)
+else:
+    print(c)
